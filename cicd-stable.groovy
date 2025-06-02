f080184e799c5cb683d215e54a4feebef4ff726b
sourabhjain5 <sourabhjain0508@gmail.com>
2025-06-02 09:00:46 -0400

    Closing review comments

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -9,6 +9,6 @@ node('linux')
       userRemoteConfigs: [[url: 'https://github.com/zopencommunity/fileport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/fileport.git'), string(name: 'PORT_DESCRIPTION', value: 'The file command is "a file type guesser", that is, a command-line tool that tells you in words what kind of data a file contains. Unlike most GUI systems, command-line *NIX systems - with this program leading the charge - don’t rely on filename extentions to tell you the type of a file, but look at the file’s actual contents. This is, of course, more reliable, but requires a bit of I/O.' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/fileport.git'), string(name: 'PORT_DESCRIPTION', value: 'The file command is "a file type guesser", that is, a command-line tool that tells you in words what kind of data a file contains. Unlike most GUI systems, command-line *NIX systems - with this program leading the charge - do not rely on filename extentions to tell you the type of a file, but look at the actual contents of the file. This is, of course, more reliable, but requires a bit of I/O.' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
   }
 }
